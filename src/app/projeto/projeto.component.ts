28eb6a5c2b6739878aeded9aaded1454b1f8511f
Diego Gusmão <diegobgusmao@gmail.com>
2023-10-30 23:15:52 -0300

    criação de header e foot e uso de data-binding

--- src/app/projeto/projeto.component.ts
+++ src/app/projeto/projeto.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projeto',
   templateUrl: './projeto.component.html',
   styleUrls: ['./projeto.component.css']
 })
-export class ProjetoComponent {
+export class ProjetoComponent implements OnInit {
+
+  listaProjetos : any[] = []
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.listaProjetos = [
+      {id:0, nome: 'projeto bvs', concluida: false},
+      {id:1, nome: 'projeto serasa', concluida: false},
+      {id:2, nome: 'projeto spc', concluida: false}
+      
+    ]
+  }
 
 }
